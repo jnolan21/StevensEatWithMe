4c1f4500e9a5fe92114701e613937dd2f897dc37
jnolan21 <142020961+jnolan21@users.noreply.github.com>
2025-04-17 11:58:40 -0400

    Add login and profile routes + views updates

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // Here is where we will list all of our routes that index.js routes to
 import userRoutes from './users.js';
 import setRoutes from './set.js';
+import profileRoutes from './profile.js';
 
 //import path from 'path';
 import {static as staticDir} from 'express';
@@ -8,6 +9,7 @@ import {static as staticDir} from 'express';
 const constructorMethod = (app) => {
 
   app.use('/users', userRoutes);
+  app.use('/profile', profileRoutes)
   app.use('/diningList', setRoutes);
   app.use('/meetupPage', setRoutes);
   app.use('/public', staticDir('public'));
