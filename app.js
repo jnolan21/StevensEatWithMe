8081992d8588489ce617ce530676a117009ea143
Jenn Alexander <jennalexander@Jenns-MacBook-Air.local>
2025-05-01 14:05:34 -0400

    tring to fix conflict merges

--- app.js
+++ app.js
@@ -77,7 +77,11 @@ app.use('/profile', (req, res, next) => {
   if (req.session.user) next(); // Authenticated user
   else return res.redirect('/users/login'); // Non-authenticated user
 });
-
+/* GET /profile/:id */
+app.use('/profile/:id', (req, res, next) => {
+  if (req.session.user) next(); // Authenticated user
+  else return res.redirect('/users/login'); // Non-authenticated user
+});
 /* GET /admin */
 app.use('/admin', (req, res, next) => {
   if (req.session.user) {
